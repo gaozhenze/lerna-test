2451f3fe562692ffa467ad535170183b6474695f
gaozhenze <gaozhenze@didiglobal.com>
2019-07-08 12:47:54 +0800

    update

--- packages/river_lerna_module1/index.js
+++ packages/river_lerna_module1/index.js
@@ -1,6 +1,5 @@
 const _ = require('lodash')
 
-console.log('test')
 
 module.exports = {
   test: function(){
--- packages/river_lerna_module2/index.js
+++ packages/river_lerna_module2/index.js
@@ -1,5 +1,7 @@
 var river_lerna_module1 = require('river_lerna_module1')
 
+console.log('test1')
+
 module.exports = {
   test: function(){
     return river_lerna_module1.test() + '_extra'
